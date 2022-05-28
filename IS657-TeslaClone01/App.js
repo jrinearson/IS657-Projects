7dacb8c7fb132f4729bda0bacd39019770ec7e21
Justin <justin@rinearson.io>
2022-05-28 10:33:28 -0700

    fix styles again

--- IS657-TeslaClone01/App.js
+++ IS657-TeslaClone01/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View styles={styles.container}>
 
     <View styles={styles.carContainer}>
 
