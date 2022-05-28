327b5cd5bbfe5f111cc60e217d13b71bc7713dcd
Justin <justin@rinearson.io>
2022-05-27 22:13:19 -0700

    .

--- IS657-TeslaClone01/App.js
+++ IS657-TeslaClone01/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <View styles={styles.carContainer}>
+
+      <View>
+        <Text>Model S</Text>
+        <Text>Starting at $69,420</Text>
+      </View>
+    </View>
+
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +28,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  carContainer: {
+
+  }
 });
